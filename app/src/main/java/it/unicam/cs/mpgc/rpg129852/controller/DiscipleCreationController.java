f2ab48c85da870c6de541e804a28e1d62a03acff
Aldo Tavoletti <aldo.tavoletti@studenti.unicam.it>
2026-07-20 11:21:18 +0200

    feat: crea la struttura base della pagina di creazione del discepolo

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg129852/controller/DiscipleCreationController.java
@@ -0,0 +1,45 @@
+package it.unicam.cs.mpgc.rpg129852.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class DiscipleCreationController {
+
+    @FXML
+    private ImageView CurrentGif;
+
+    @FXML
+    private ChoiceBox<?> JobSelector;
+
+    @FXML
+    private TextField NameField;
+
+    @FXML
+    private Button NextGifButton;
+
+    @FXML
+    private Button PreviousGifButton;
+
+    @FXML
+    private Button StartGameButton;
+
+    @FXML
+    void showNextGif(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showPreviousGif(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startGame(ActionEvent event) {
+
+    }
+
+}
