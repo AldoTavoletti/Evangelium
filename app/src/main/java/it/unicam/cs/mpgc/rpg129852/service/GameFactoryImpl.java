9996a661bd7969b3ad0a4f3bf09d0d00b8febee7
Aldo Tavoletti <aldo.tavoletti@studenti.unicam.it>
2026-07-28 09:52:45 +0200

    fix: aggiusta GameFactoryImpl dopo la modifica dell'interfaccia GameFactory

--- app/src/main/java/it/unicam/cs/mpgc/rpg129852/service/GameFactoryImpl.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg129852/service/GameFactoryImpl.java
@@ -7,9 +7,9 @@ import it.unicam.cs.mpgc.rpg129852.model.GameState;
 public class GameFactoryImpl implements GameFactory {
 
     @Override
-    public Game create(NewGameRequest request, String saveName) {
+    public Game create(NewGameRequest request) {
         DiscipleData discipleData = new DiscipleData(request.discipleName(), request.job(), request.color());
         GameState gameState = new GameState(discipleData);
-        return new Game(saveName, gameState);
+        return new Game(request.saveName(), gameState);
     }
 }
